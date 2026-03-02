6887b55e087d1fb125b7a264f58070a72fc01579
Aoki ryo <pyryo1230@gmail.com>
2026-03-02 22:32:50 +0900

    post get

--- src/main/java/student/maneger/student/manager/Application.java
+++ src/main/java/student/maneger/student/manager/Application.java
@@ -3,19 +3,35 @@ package student.maneger.student.manager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class Application {
 
+	private String name = "Aoki Ryo";
+	private String age = "31";
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String hello() {
-        return "Hello World!";
+	@GetMapping("/studentInfo")
+	public String getStudentInfo() {
+        return name + " " + age + "歳";
+	}
+
+	@PostMapping("/studentInfo")
+	public void setStudentInfo(String name,  String age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	@PostMapping("/studentName")
+	public void setStudentName(String name) {
+		if (name.isEmpty()) {this.name = "Aoki Ryo";}
+		this.name = name;
 	}
 
 }
