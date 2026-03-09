37fb4dfe3e52b44596293f0c111aa02ef8e82906
ryo <pyryo1230@gmail.com>
2026-03-09 11:03:20 +0900

    MyBatisを用いたMySQLとの連携

--- src/main/java/student/maneger/student/manager/Application.java
+++ src/main/java/student/maneger/student/manager/Application.java
@@ -1,37 +1,65 @@
 package student.maneger.student.manager;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 @RestController
 public class Application {
 
-	private String name = "Aoki Ryo";
-	private String age = "31";
+	@Autowired
+	private StudentRepository repository;
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
 	@GetMapping("/studentInfo")
-	public String getStudentInfo() {
-        return name + " " + age + "歳";
+	public String getStudentInfo(@RequestParam String name) {
+		Student student = repository.searchByName(name);
+		if (student == null) {
+			return name + " is not found";
+		}
+        return student.getName() + " " + student.getAge() + "歳";
+	}
+
+	@GetMapping("/studentList")
+	public String getStudentList() {
+		List<Student> studentList = repository.searchAll();
+		StringBuilder studentListString = new StringBuilder();
+		for (Student student : studentList) {
+			studentListString
+				.append(student.getName())
+				.append(", ")
+				.append(student.getAge())
+				.append("\n");
+		}
+		return  studentListString.toString();
 	}
 
 	@PostMapping("/studentInfo")
-	public void setStudentInfo(String name,  String age) {
-		this.name = name;
-		this.age = age;
+	public void registerStudentInfo(String name, int age) {
+		repository.registerStudent(name, age);
 	}
 
-	@PostMapping("/studentName")
-	public void setStudentName(String name) {
-		if (name.isEmpty()) {this.name = "Aoki Ryo";}
-		this.name = name;
+	@PatchMapping("/studentInfo")
+	public void updateStudentName(String name, int age) {
+		repository.updateStudentAge(name, age);
+	}
+
+	@DeleteMapping("/studentInfo")
+	public void deleteStudentInfo(@RequestParam String name) {
+		repository.deleteStudent(name);
 	}
 
 }
