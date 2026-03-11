76bfc59daaef4a6715eb1d4edb50b8cc28018028
ryo <pyryo1230@gmail.com>
2026-03-12 00:14:37 +0900

    24_課題作成

--- src/main/java/studentManager/service/StudentService.java
+++ src/main/java/studentManager/service/StudentService.java
@@ -1,6 +1,7 @@
 package studentManager.service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,12 +21,26 @@ public class StudentService {
 
     @GetMapping
     public List<Student> getStudentList() {
-        return repository.searchAll();
+        List<Student> studentList;
+        studentList = repository.searchAll().stream()
+            .filter(student -> student.getAge() >= 30).collect(Collectors.toList());
+        return studentList;
     }
+
+    // 絞り込みをする。年齢が30代以上の人のみを抽出する
+    // service内で完結する。
+    // 抽出したリストをコントローラーに返す
 
     @GetMapping
     public List<StudentCourse> getStudentCourseList() {
-        return repository.getStudentCourseList();
+        List<StudentCourse> studentCourseList;
+        studentCourseList = repository.getStudentCourseList().stream()
+            .filter(studentCourse -> studentCourse.getCourseName().equals("Java"))
+            .collect(Collectors.toList());
+        return studentCourseList;
     }
+
+    // 絞り込み検索でJavaコースのみコース情報のみを抽出する
+    // 抽出したリストをコントローラーに返す
 
 }
